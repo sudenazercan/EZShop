06b335e62551bee49d50352d782135c80f888c98
sudenazercan <sudenaz162002@gmail.com>
2024-10-10 09:56:18 +0300

    initial commit

---
+++ BLL/DAL/Category.cs
@@ -0,0 +1,13 @@
+#nullable disable
+namespace BLL.DAL;
+
+public class Category
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+    
+    public List<Product> Products { get; set; }
+}
---
+++ BLL/DAL/Students.cs
@@ -0,0 +1,15 @@
+namespace BLL.DAL;
+
+public class Students
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Surname { get; set; }
+    
+    public DateTime BirthDate { get; set; }
+    
+    public double GPA { get; set; }
+    
+}
